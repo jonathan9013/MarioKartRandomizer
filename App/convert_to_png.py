8a1d211274f68582228fa90593528c447004746a
JosiahCannon <jrolfecannon@gmail.com>
2022-08-11 09:36:42 -0400

    file to convert images to png

---
+++ App/convert_to_png.py
@@ -0,0 +1,15 @@
+from email.mime import image
+import os
+from os import listdir
+from PIL import Image
+
+# get the path/directory
+folder_dir = "C:\Dev\MarioKartRandomizerApp\MarioKartRandomizer\\app\media\\tires"
+for images in os.listdir(folder_dir):
+
+    # check if the image ends with png
+    if (images.endswith(".png")):
+        image_dir = folder_dir + '\\' + images
+        im1 = Image.open(image_dir, mode='r')
+        im1.save(image_dir, mode='r')
+        print(images)
