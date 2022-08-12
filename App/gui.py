7915195660dfe7a2e5b349dcc8351701cb06f363
JosiahCannon <jrolfecannon@gmail.com>
2022-08-12 17:02:48 -0400

    remove match cases; update logic to get image paths

--- App/gui.py
+++ App/gui.py
@@ -4,175 +4,9 @@ from main import *
 from Track import Track
 from Character import Character
 from Cup import Cup
-
-def get_kart_icon(currKart):
-    match currKart:
-        # atvs
-        case "Bone Rattler":
-            return "app\media\\atvs\MK8BoneRattler.png"
-        case "Inkstriker":
-            return "app\media\\atvs\MK8DX_Inkstriker.png"
-        case "Splat Buggy":
-            return "app\media\\atvs\MK8DX_Splat_Buggy.png"
-        case "Standard ATV":
-            return "app\media\\atvs\StandardATVBodyMK8.png"
-        case "Teddy Buggy":
-            return "app\media\\atvs\TeddyBuggyBodyMK8.png"
-        case "Wild Wiggler":
-            return "app\media\\atvs\WildWigglerBodyMK8.png"
-        # standard bikes
-        case "Flame Rider":
-            return "app\media\\bikes\FlameRiderBodyMK8.png"
-        case "City Tripper":
-            return "app\media\\bikes\MK8_Light-Green_City_Tripper.png"
-        case "Master Cycle Zero":
-            return "app\media\\bikes\MK8D_Master_Cycle_Zero.png"
-        case "Mr. Scooty":
-            return "app\media\\bikes\MrScootyBodyMK8.png"
-        case "Standard Bike":
-            return "app\media\\bikes\StandardBikeBodyMK8.png"
-        case "The Duke":
-            return "app\media\\bikes\TheDukeBodyMK8.png"
-        case "Varmint":
-            return "app\media\\bikes\VarmintBodyMK8.png"
-        # sport bikes
-        case "Comet":
-            return "app\media\sport_bikes\CometBodyMK8.png"
-        case "Jet Bike":
-            return "app\media\sport_bikes\JetBikeBodyMK8.png"
-        case "Master Cycle":
-            return "app\media\sport_bikes\MK8MasterCycle.png"
-        case "Sport Bike":
-            return "app\media\sport_bikes\SportBikeBodyMK8.png"
-        case "Yoshi Bike":
-            return "app\media\sport_bikes\YoshiBikeBodyMK8.png"
-        # karts
-        case "300 SL Roadster":
-            return "app\media\karts\\300SLRoadster_MK8.png"
-        case "Badwagon":
-            return "app\media\karts\BadwagonBodyMK8.png"
-        case "Biddybuggy":
-            return "app\media\karts\BiddybuggyBodyMK8.png"
-        case "Cat Cruiser":
-            return "app\media\karts\CatCruiserBodyMK8.png"
-        case "Circuit Special":
-            return "app\media\karts\CircuitSpecialBodyMK8.png"
-        case "GLA":
-            return "app\media\karts\GLA-MK8.png"
-        case "Gold Standard":
-            return "app\media\karts\Gold_Standard.png"
-        case "Landship":
-            return "app\media\karts\LandshipBodyMK8.png"
-        case "Mach 8":
-            return "app\media\karts\Mach8BodyMK8.png"
-        case "Tanooki Kart":
-            return "app\media\karts\MK8_Tanooki_Buggy_Sprite.png"
-        case "Blue Falcon":
-            return "app\media\karts\MK8BlueFalcon.png"
-        case "Koopa Clown":
-            return "app\media\karts\MK8DX_Koopa_Clown.png"
-        case "P-Wing":
-            return "app\media\karts\MK8PWing.png"
-        case "Streetle":
-            return "app\media\karts\MK8Streetle.png"
-        case "Pipe Frame":
-            return "app\media\karts\PipeFrameBodyMK8.png"
-        case "Prancer":
-            return "app\media\karts\PrancerBodyMK8.png"
-        case "Sneeker":
-            return "app\media\karts\SneakerBodyMK8.png"
-        case "Sports Coupe":
-            return "app\media\karts\SportsCoupeMK8.png"
-        case "Standard Kart":
-            return "app\media\karts\StandardKartBodyMK8.png"
-        case "Steel Driver":
-            return "app\media\karts\Steel_Driver.png"
-        case "Tri-Speeder":
-            return "app\media\karts\TrispeederBodyMK8.png"
-        case "W 25 Silver Arrow":
-            return "app\media\karts\W25SilverArrow-MK8.png"
-        case "B Dasher":
-            return "app\media\karts\ZeldaMK8Bdasher.png"
-
-def get_tire_icon(currTire):
-    match currTire:
-        case "Azure Roller":
-            return "app\media\\tires\AzureRollerTiresMK8.png"
-        case "Blue Standard":
-            return "app\media\\tires\Blue_Standard.png"
-        case "Button":
-            return "app\media\\tires\ButtonTiresMK8.png"
-        case "Crimson Slim":
-            return "app\media\\tires\CrimsonSlimTiresMK8.png"
-        case "Cushion":
-            return "app\media\\tires\CushionTiresMK8.png"
-        case "Cyber Slick":
-            return "app\media\\tires\CyberSlickTiresMK8.png"
-        case "GLA Tires":
-            return "app\media\\tires\GLATires-MK8.png"
-        case "Gold Tires":
-            return "app\media\\tires\Gold_Tires_MK8.png"
-        case "Hot Monster":
-            return "app\media\\tires\HotMonsterTiresMK8.png"
-        case "Leaf Tires":
-            return "app\media\\tires\Leaf_Tires_MK8.png"
-        case "Metal":
-            return "app\media\\tires\MetalTiresMK8.png"
-        case "Triforce Tires":
-            return "app\media\\tires\MK8-TriforceTires.png"
-        case "Ancient Tires":
-            return "app\media\\tires\MK8D_Ancient_Tires.png"
-        case "Monster":
-            return "app\media\\tires\MonsterTiresMK8.png"
-        case "Off-Road":
-            return "app\media\\tires\Off-Road.png"
-        case "Retro Off-Road":
-            return "app\media\\tires\Retro_Off-Road.png"
-        case "Roller":
-            return "app\media\\tires\RollerTiresMK8.png"
-        case "Slick":
-            return "app\media\\tires\SlickTiresMK8.png"
-        case "Slim":
-            return "app\media\\tires\SlimTiresMK8.png"
-        case "Sponge":
-            return "app\media\\tires\SpongeTiresMK8.png"
-        case "Standard":
-            return "app\media\\tires\StandardTiresMK8.png"
-        case "Wood":
-            return "app\media\\tires\WoodTiresMK8.png"
-
-def get_glider_icon(currGlider):
-    match currGlider:
-        case "Bowser Kite":
-            return "app\media\gliders\BowserKiteMK8.png"
-        case "Cloud Glider":
-            return "app\media\gliders\Cloud_Glider.png"
-        case "Flower Glider":
-            return "app\media\gliders\FlowerGliderMK8.png"
-        case "Gold Glider":
-            return "app\media\gliders\GoldGliderMK8.png"
-        case "Hylian Kite":
-            return "app\media\gliders\MK8-HylianKite.png"
-        case "Paraglider":
-            return "app\media\gliders\MK8D_Paraglider.png"
-        case "MKTV Parafoil":
-            return "app\media\gliders\MKTVParafoilGliderMK8.png"
-        case "Paper Glider":
-            return "app\media\gliders\PaperGliderIcon-MK8.png"
-        case "Parachute":
-            return "app\media\gliders\ParachuteGliderMK8.png"
-        case "Parafoil":
-            return "app\media\gliders\ParafoilGliderMK8.png"
-        case "Peach Parasol":
-            return "app\media\gliders\PeachParasolGliderMK8.png"
-        case "Plane Glider":
-            return "app\media\gliders\PlaneGliderMK8.png"
-        case "Super Glider":
-            return "app\media\gliders\SuperGliderMK8.png"
-        case "Waddle Wing":
-            return "app\media\gliders\WaddleWingMK8.png"
-        case "Wario Wing":
-            return "app\media\gliders\WarioWingMK8.png"
+from kart import Kart
+from glider import Glider
+from tire import Tire
 
 def check_valid(type_array, size_array):
     kart_exists = False
@@ -214,9 +48,9 @@ currCup = getCurrentCup()
 
 # call case match functions to get correct file paths
 char_icon = currCharacter.image
-kart_icon = get_kart_icon(currKart)
-tire_icon = get_tire_icon(currTire)
-glider_icon = get_glider_icon(currGlider)
+kart_icon = currKart.image
+tire_icon = currTire.image
+glider_icon = currGlider.image
 track_icon1 = currTrack.image
 cup_icon = currCup.image
 
@@ -366,7 +200,7 @@ while True:
     if event == '-REROLLKART-':
         randomizeAspect("kart")
         currKart = getCurrentKart()
-        kart_icon = get_kart_icon(currKart)
+        kart_icon = currKart.image
 
         element = window[event]
         window['kart_image'].update(filename=kart_icon)
@@ -374,7 +208,7 @@ while True:
     if event == '-REROLLTIRE-':
         randomizeAspect("tires")
         currTire = getCurrentTire()
-        tire_icon = get_tire_icon(currTire)
+        tire_icon = currTire.image
 
         element = window[event]
         window['tire_image'].update(filename=tire_icon)
@@ -382,7 +216,7 @@ while True:
     if event == '-REROLLGLIDER-':
         randomizeAspect("glider")
         currGlider = getCurrentGlider()
-        glider_icon = get_glider_icon(currGlider)
+        glider_icon = currGlider.image
 
         element = window[event]
         window['glider_image'].update(filename=glider_icon)
@@ -435,13 +269,13 @@ while True:
             char_icon = currCharacter.image
 
             currKart = getCurrentKart()
-            kart_icon = get_kart_icon(currKart)
+            kart_icon = currKart.image
 
             currTire = getCurrentTire()
-            tire_icon = get_tire_icon(currTire)
+            tire_icon = currTire.image
 
             currGlider = getCurrentGlider()
-            glider_icon = get_glider_icon(currGlider)
+            glider_icon = currGlider.image
 
             window['char_image'].update(filename=char_icon)
             window['kart_image'].update(filename=kart_icon)
