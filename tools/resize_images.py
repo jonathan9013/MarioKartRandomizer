06af707535169c19f792aafb541f9b3e0612168d
jonathan9013 <jlaustin13@gmail.com>
2022-12-08 09:56:21 -0500

    added new tracks

--- tools/resize_images.py
+++ tools/resize_images.py
@@ -3,22 +3,22 @@ from os import listdir
 from PIL import Image
 
 # get the path/directory
-folder_dir = "C:\Dev\MarioKartRandomizerApp\MarioKartRandomizer\\app\media\\"
+folder_dir = "app\media\\tracks"
 for images in os.listdir(folder_dir):
     image_dir = folder_dir + '\\' + images
     im1 = Image.open(image_dir)
 
     # use base width
-    # basewidth = 120
-    # wpercent = (basewidth/float(im1.size[0]))
-    # hsize = int((float(im1.size[1])*float(wpercent)))
-    # im1 = im1.resize((basewidth, hsize), Image.ANTIALIAS)
+    basewidth = 120
+    wpercent = (basewidth/float(im1.size[0]))
+    hsize = int((float(im1.size[1])*float(wpercent)))
+    im1 = im1.resize((basewidth, hsize), Image.ANTIALIAS)
 
     # use base height
-    baseheight = 50
-    wpercent = (baseheight/float(im1.size[1]))
-    wsize = int((float(im1.size[0])*float(wpercent)))
-    im1 = im1.resize((wsize, baseheight), Image.ANTIALIAS)
+    # baseheight = 80
+    # wpercent = (baseheight/float(im1.size[1]))
+    # wsize = int((float(im1.size[0])*float(wpercent)))
+    # im1 = im1.resize((wsize, baseheight), Image.ANTIALIAS)
 
     im1.save(image_dir, mode='r')
     print(images)
