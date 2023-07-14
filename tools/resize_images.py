65663ef50aeb92264d59465a4584372a3661f703
jonathan9013 <jlaustin13@gmail.com>
2023-07-14 18:50:41 -0400

    added wave 5 characters and tracks

--- tools/resize_images.py
+++ tools/resize_images.py
@@ -3,13 +3,13 @@ from os import listdir
 from PIL import Image
 
 # get the path/directory
-folder_dir = "app\media\\cups"
+folder_dir = "app\media\\tracks"
 for images in os.listdir(folder_dir):
     image_dir = folder_dir + '\\' + images
     im1 = Image.open(image_dir)
 
     # use base width
-    basewidth = 106
+    basewidth = 120
     wpercent = (basewidth/float(im1.size[0]))
     hsize = int((float(im1.size[1])*float(wpercent)))
     im1 = im1.resize((basewidth, hsize), Image.ANTIALIAS)
